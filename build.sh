acdb789a2da8daefd37dc3ede9f3c2ae5ff8327a
Nathan <90388021+SodiumBismuth@users.noreply.github.com>
2024-11-27 11:46:56 +0100

    Création d'une fonction build

---
+++ build.sh
@@ -0,0 +1,6 @@
+echo "Création d'un exécutable Linux"
+g++ main.cpp -o GameOfLife -lsfml-graphics -lsfml-window -lsfml-system
+echo "Terminé !\nCréation d'un exécutable Windows"
+x86_64-w64-mingw32-g++ main.cpp -o GameOfLife.exe -lsfml-graphics -lsfml-window -lsfml-system
+echo "Terminé !\nNote : La version de windows n'est pas une exécutable signé, vous aurez donc un message de protection venant de Windows"
+pause
