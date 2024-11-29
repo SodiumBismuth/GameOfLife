d3b6627887d5607a1da32169a287a214de7b70af
Nathan <90388021+SodiumBismuth@users.noreply.github.com>
2024-11-29 10:53:27 +0100

    Classe GOLRule
    Co-authored-by: Baptiste-01 <Baptiste-01@users.noreply.github.com>

--- golrule.h
+++ golrule.h
@@ -2,11 +2,31 @@
 #define GOLRULE_H
 
 #include <iostream>
+#include <vector>
 #include "rule.h"
+using namespace std;
 
 class GOLRule : public Rule {
 public:
-    void apply_rule(Cell* cell, vector<Cell*> neighbors)
+    void apply_rule(Cell* cell, vector<Cell*> neighbors) override{ //Applique la règle basique de gol
+        int alive_count=0; //Compteur de cellule vivante
+        for(Cell* neighbor : neighbors){ //boucle pour regarder l'état des voisins
+            if (neighbor->get_state() == 1)
+             ++alive_count;
+        }
+        if(cell->get_state() == 1){ 
+            if(alive_count < 2 || alive_count > 3 ){ //cellule vivante devient morte
+                cell->set_state(0);
+            } else {
+                cell->set_state(1); //cellule vivante reste vivante
+            }
+        }
+        else{ 
+            if(alive_count ==3){
+                cell->set_state(1); //cellule morte devient vivante
+            }
+        }
+    } 
 };
 
 #endif
