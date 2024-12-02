23dcb61850cb3c0ccab9db13140c8e3b0715149e
Nathan <90388021+SodiumBismuth@users.noreply.github.com>
2024-12-02 10:34:43 +0100

    Faire des pointeurs était une mauvaise idée...
    Co-authored-by: Baptiste-01 <Baptiste-01@users.noreply.github.com>

--- grid.h
+++ grid.h
@@ -10,13 +10,13 @@
 
 using namespace std;
 
-class Grid{
+class Grid{ //classe permettant de réaliser une grille avec des cellules
 private:
-    vector<vector<Cell*>> grid;
+    vector<vector<Cell>> grid;
     int h, w;
 public:
     Grid(int w, int h, string grid_data) : w(w), h(h){//construteur
-        vector<vector<Cell*>> grid(h, vector<Cell*>(w, nullptr));
+        vector<vector<Cell>> grid(h, vector<Cell>(w));
 
     // Utiliser un istringstream pour lire les données
         istringstream iss(grid_data);
@@ -26,8 +26,8 @@ public:
             for (int j = 0; j < w; ++j) {
                 if (iss >> value) {
                     // Créer une nouvelle Cell et définir son état
-                    grid[i][j] = new Cell();
-                    grid[i][j]->set_state(value);
+                    grid[i][j] = Cell();
+                    grid[i][j].set_state(value);
                 } else {
                     throw 4;
                 }
@@ -35,10 +35,30 @@ public:
         }
     }  
 
-    void update(Rule* rule) {}//change le statut de la cellule
-    
-    vector<vector<Cell*>>get_grid(); //retourne le statut de la cellule
+    bool update(Rule* rule){ //change le statut de la cellule
+        vector<vector<Cell>>next_grid = grid; 
+    //Permet de mettre a jour l'état de toutes les cellules sans prendre celles déjà mise à jour
+        for(int i = 0; i < h; i++){
+            for(int j = 0; j < w; j++){ // Parcours toutes les cellules
+                vector<Cell> neighbors;
+                for (int di = -1; di <=1; di++){ // regarde chaque voisin
+                    for (int dj = -1; dj<=1; dj++){
+                        if (di == 0 && dj == 0) continue; // evite la cellule
+                        int ni = i + di, nj = j +dj;
+                        if (ni >=0 && ni < h && nj >= 0 && nj < w){
+                            neighbors.push_back(grid[ni][nj]);
+                        }
+                    }
+                }
+                rule->apply_rule(next_grid[i][j], neighbors); // applique les regles
+            }
+        }
+        grid = next_grid; // Mise a jour de la grille
+        return grid == next_grid;
+    }
+    vector<vector<Cell>>get_grid() {return grid;} ; //retourne la grille
 
+    void save_file();
 };
 
 #endif
